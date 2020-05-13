3ac4da9e35988d66445c7fb0aaa1876b9565f6a2
gonzalo703 <gonzalo703@hotmail.com>
2020-05-13 18:41:30 -0300

    cambios en routes

--- site/routes/login.js
+++ site/routes/login.js
@@ -1,8 +1,9 @@
 var express = require("express");
 var router = express.Router();
 
+/* GET home page. */
 router.get("/", function (req, res) {
-  res.render("register");
+  res.render("login");
 });
 
 module.exports = router;
