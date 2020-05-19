f89f0a6d3455b47c314c08c91b54cd507a086567
Rocksyro <rociocarle@yahoo.com.ar>
2020-05-19 19:35:20 -0300

    Creando rutas

---
+++ site/controllers/mainController.js
@@ -0,0 +1,9 @@
+module.exports={
+    root:function (req, res, next) {
+        res.render('index', {title: 'Home'});
+    },
+
+    register:function (req, res, next){
+        res.render('register', {title: 'Registro'})
+    }
+}
--- site/routes/index.js
+++ site/routes/index.js
@@ -1,9 +1,11 @@
 var express = require("express");
 var router = express.Router();
+const mainController = require ('../controllers/mainController');
 
 /* GET productCart page. */
-router.get("/", function (req, res) {
+router.get("/", mainController.root);
   res.render("index");
-});
+
+router.get("/register", mainController.register);
 
 module.exports = router;
