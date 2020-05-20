247246f149c0ff973ec6fb623d2d1f71cacb395e
guillecipri <guillecipriani@gmail.com>
2020-05-19 21:04:11 -0300

    subo cambios en partials, gitignore y carpeta src

--- site/routes/productAdd.js
+++
@@ -1,9 +0,0 @@
-var express = require("express");
-var router = express.Router();
-
-/* GET productAdd page. */
-router.get("/", function (req, res) {
-  res.render("productAdd");
-});
-
-module.exports = router;
