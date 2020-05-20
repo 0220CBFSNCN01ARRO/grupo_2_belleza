5cb9b57088eb11a5ab30a930cc924715661770a0
guillecipri <guillecipriani@gmail.com>
2020-05-20 18:20:07 -0300

    subo de nuevo src

---
+++ site/src/routes/productDetail.js
@@ -0,0 +1,9 @@
+var express = require("express");
+var router = express.Router();
+
+/* GET productDetail page. */
+router.get("/", function (req, res) {
+  res.render("productDetail");
+});
+
+module.exports = router;
