e0d78c7fe7395073057c813048757ac8ee18eb20
Gonzalo <gonzalo703@hotmail.com>
2020-05-23 17:32:34 -0300

    algo

--- site/src/controllers/productsController.js
+++ site/src/controllers/productsController.js
@@ -1,31 +1,30 @@
-const fs = require('fs');
-const path = require('path');
+const fs = require("fs");
+const path = require("path");
 
-
-var controller={
-products: function (req, res, next){
-    res.render('products', {title: 'Productos'});
-},
-detail: function (req, res, next) {
-    res.render('productDetail', {title: 'Detalle de producto'});
-},
-create: function (req, res, next) {
-    res.render('productAdd', {title: 'Crear Producto'});    
-},
-edit: function (req, res, next) {
-    res.render('products', {title: 'Editar Producto'});
-},
-store: function (req, res, next) {
-    res.render()    //Accion de crear y guardar prod nuevo//
-},
-delete: function (req, res, next) {
+var controller = {
+  products: function (req, res, next) {
+    res.render("products", { title: "Productos" });
+  },
+  detail: function (req, res, next) {
+    res.render("detail", { title: "Detalle de producto" });
+  },
+  create: function (req, res, next) {
+    res.render("productAdd", { title: "Crear Producto" });
+  },
+  edit: function (req, res, next) {
+    res.render("products", { title: "Editar Producto" });
+  },
+  store: function (req, res, next) {
+    res.render(); //Accion de crear y guardar prod nuevo//
+  },
+  delete: function (req, res, next) {
     // let final = products.filter(prod=> prod.id != req.params.productId)
     // fs.writeFileSync(productsFilePath, JSON.stringify(final, null, ' '));
-    res.redirect('/products')
-},
-update: function (req, res, next) {
-    res.redirect('/products')
-}
-}
+    res.redirect("/products");
+  },
+  update: function (req, res, next) {
+    res.redirect("/products");
+  },
+};
 
 module.exports = controller;
