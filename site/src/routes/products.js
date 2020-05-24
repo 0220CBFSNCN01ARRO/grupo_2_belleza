64608e1461194aefb00f98ef5aaccbf6ecff18ec
Rocksyro <rociocarle@yahoo.com.ar>
2020-05-24 17:32:17 -0300

    sdf

--- site/src/routes/products.js
+++ site/src/routes/products.js
@@ -1,15 +1,15 @@
-// var express = require('express');
-// var router = express.Router();
-// var productsController = require('../controllers/productsController');
+var express = require('express');
+var router = express.Router();
+var productsController = require('../controllers/productsController');
 
-// // router.get('/products', productsController.products); 
-// // router.get('/products/detail/:id/:category', productsController.detail);
+router.get('/', productsController.products); 
+router.get('/detail', productsController.detail);
 
-// // router.get('/products/create', productsController.create);//Vista del form para crear prod//
-// // router.post('/products/create', productsController.store);//Acción de crear y guardar//
+router.get('/productAdd', productsController.create);//Vista del form para crear prod//
+router.post('/create', productsController.store);//Acción de crear y guardar//
 
-// // router.get('/products/:id/edit', productsController.edit);//Vista del form para editar prod//
-// // router.put('/products/:id/edit', productsController.update);//Acción d mandar la modificacion (Modificar el formulario)//
-// // router.delete('/products/:id/delete', productsController.delete);
+router.get('/:id/edit', productsController.edit);//Vista del form para editar prod//
+router.put('/:id/edit', productsController.update);//Acción d mandar la modificacion (Modificar el formulario)//
+router.delete('/:id/delete', productsController.delete);
 
-// module.exports = router;
+module.exports = router;
