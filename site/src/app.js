4aec615a748e88cbfbc2829a0fa0b2c70aa8a0c1
Gonzalo <gonzalo703@hotmail.com>
2020-06-15 21:49:56 -0300

    algo

--- site/src/app.js
+++ site/src/app.js
@@ -25,7 +25,7 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, "../public")));
 app.use(methodOverride("_method"));
-app.use(session({ secret: "SecretBeauty" }));
+// app.use(session({ secret: "SecretBeauty" }));
 
 app.use("/", indexRouter);
 app.use("/products", productsRouter);
