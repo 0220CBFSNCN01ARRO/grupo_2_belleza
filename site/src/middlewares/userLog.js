3ed5d92e9ef61eec53879f4eaf3c668f60a49ec5
Rocksyro <rociocarle@yahoo.com.ar>
2020-06-24 19:46:20 -0300

    cambios middlewares, controllers

--- site/src/middlewares/userLog.js
+++ site/src/middlewares/userLog.js
@@ -1,7 +1,17 @@
 module.exports = (req, res, next) => {
   if (req.session.user) {
-    next();
-  } else {
-    res.redirect("/");
-  }
+    if (req.params.id){
+      if(req.params.id == req.session.user.id){
+          next()
+      } else {
+          res.redirect(`/profile/${req.session.user.id}`);
+      };
+  // si esta logueado pero no entra a info de ningun usuario en particular
+  }else {
+      next()
+  };
+// si NO esta logueado
+}else {
+  res.redirect('/');
 };
+}
