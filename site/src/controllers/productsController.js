eeb06ac209739b5dd36bfc7e95568c57adb16d64
Rocksyro <rociocarle@yahoo.com.ar>
2020-06-28 19:32:57 -0300

    cambio controlador prods

--- site/src/controllers/productsController.js
+++ site/src/controllers/productsController.js
@@ -1,79 +1,99 @@
 const fs = require("fs");
 const path = require("path");
+const db = require('../database/models');
 
-const productsFilePath = path.join(__dirname, "../data/Products.json");
-const products = JSON.parse(fs.readFileSync(productsFilePath, "utf-8"));
+// const productsFilePath = path.join(__dirname, "../data/Products.json");
+// const products = JSON.parse(fs.readFileSync(productsFilePath, "utf-8"));
 
 const controller = {
   // VER TODOS LOS PRODUCTOS
-  products: function (req, res) {
-    res.render("products", { products });
+  products: (req, res) => {
+  db.product
+            .findAll()
+            .then(products => {
+                res.render('/products', { productos });
+            })
+            .catch(error => console.log(error));
   },
 
   // VER DETALLE DE CADA PRODUCTO
-  detail: function (req, res) {
-    let producto = products.find((prod) => prod.id == req.params.productId);
-    res.render("productDetail", { producto });
+  detail: (req, res) => {
+    db.productos
+        .findByPk(req.params.id, { include: ['categoriaproducto']})
+        .then(productos => {
+            if(productos) {
+                res.render('productDetail', { productos });
+            } else {
+                res.render('error');
+            }
+        })
+        .catch(error => console.log(error));
   },
 
   // CREAR UN PRODUCTO NUEVO
-  create: function (req, res) {
-    res.render("productAdd", { title: "Crear Producto" });
+  create: (req, res) => {
+  db.productos
+  .findAll()
+  .then(productos => {
+      res.render('productAdd');
+  })
+  .catch(error => console.log(error));
   },
   //Accion de crear y guardar prod nuevo
-  store: function (req, res, next) {
-    // elegir id
-    let ids = products.map((prod) => prod.id);
-    let id = Math.max(...ids) + 1;
-
-    // crear producto con datos del formulario
-    req.body.price = Number(req.body.price);
-    req.body.cantidad = Number(req.body.cantidad);
-
-    // actualizar datos del prod nuevo
-    let productoNuevo = {
-      id: id,
-      ...req.body,
-      image: req.files[0].filename
-    };
-
-    // agregar producto nuevo al array
-    let final = [...products, productoNuevo];
-
-    // se guarda en json
-    fs.writeFileSync(productsFilePath, JSON.stringify(final, null, " "));
-
-    //redirecciono
-    res.redirect("/products");
+  store: (req, res) => {
+    product = req.body;
+    product.image = req.file ? req.file.filename : '';
+        
+    db.product
+      .create(product)
+      .then(storedProduct => {
+          //storedProduct.addTags(req.bokeywords.split(' '))
+          return res.redirect(`/products/${storedProduct.id}`)
+      })
+      .catch(error => { console.log(error)});
   },
 
   // EDITAR UN PRODUCTO EXISTENTE
-  edit: function (req, res) {
-    let producto = products.find((prod) => prod.id == req.params.productId);
-    res.render("productEdit", { producto });
+  edit: (req, res) => {
+  const producto = db.productos.findByPk(req.params.id);
+  const categorias = db.categoriaproducto.findAll();
+
+  Promise
+      .all([producto, categorias])
+      .then(responses => {
+          if(responses[0]) {
+              console.log(responses[0].dataValues);
+              res.render('productEdit', { producto: responses[0], categorias: responses[1] });
+          } else {
+              res.render('error');
+          }
+      })
+      .catch(error => console.log(error));
   },
   // ACCION DE EDITAR
-  update: function (req, res) {
-    req.body.price = Number(req.body.price);
+  update: (req, res) => {
 
-    let final = products.map(prod => {
-      if (prod.id == req.params.productId) {
-        return {
-          id: prod.id,
-          ...req.body,
-          image: prod.image,
-        };
-      } else {
-        return prod;
-      }
-    });
+    producto = req.body;
+    
+    producto.image = req.params.image ? req.body.image : req.body.oldImage;
+    delete product.oldImage;
 
-    // se guarda en el Json
-    fs.writeFileSync(productsFilePath, JSON.stringify(final, null, " "));
-    // redirecciono a PRODUCTS
-    res.redirect("/products");
-  },
-
+    // product.keywords = product.keywords.split(' ');
+    
+    db.productos
+        .update(producto, {
+            where: {
+                id: req.params.id
+            }
+        })
+        .then(updatedProducto => {
+            // Guardar tags
+            // updatedProduct.addTags()
+            res.redirect(`/productDetail/${req.params.id}`)
+        })
+        .catch(error => { console.log(error) })
+    
+},
   // CARRITO DE COMPRAS
   carrito: function (req, res) {
     res.render("productCart", { title: "Carrito de compras" });
@@ -84,14 +104,25 @@ const controller = {
   },
 
   // BORRAR UN PRODUCTO
-  destroy: function (req, res) {
-    // 1- eliminar el producto seleccionado
-    let final = products.filter(prod=> prod.id != req.params.productId)
-    // 2- actualizar el json sin el producto
-    fs.writeFileSync(productsFilePath, JSON.stringify(final, null, ' '));
-    // 3- redirecciono a todos los productos
-    res.redirect("/products");
-  },
+  destroy: (req, res) => {
+    db.productos
+        .findByPk(req.params.id)
+        // Si el registro existe
+        .then(async producto => {
+            // Lo borramos
+            await db.productos.destroy({ where: { id: req.params.id } });
+            
+            // y además borramos la imagen asociada
+            const imagePath = path.resolve(__dirname, '../../public/img/products', producto.image);
+            if (fs.existsSync(imagePath)) {
+                fs.unlinkSync(imagePath);
+            }
+
+            // luego volvemos al listado
+            res.redirect(`/products/`)
+        })
+        .catch(error => console.log(error));
+}
 };
 
 module.exports = controller;
