c189fafe6084e784e938e67537bda8cdf89c51a6
Gonzalo <gonzalo703@hotmail.com>
2020-07-16 16:35:03 -0300

    algo

--- site/src/middlewares/auth.js
+++ site/src/middlewares/auth.js
@@ -1,25 +1,16 @@
 const db = require("../database/models");
 
 module.exports = (req, res, next) => {
-<<<<<<< HEAD
   if (req.cookies.cookieuser) {
     return res.render("users/login", {
       title: "Perfil usuario",
-      user: req.cookies.cookieuser,
+      usuario: req.cookies.cookieuser,
     });
   } else if (req.session.usuario) {
     return res.render("users/login", {
       title: "Perfil usuario",
-      user: req.session.usuario,
+      usuario: req.session.usuario,
     });
   }
-=======
- 
-  if(req.cookies.cookieuser){
-    return res.render ('users/login', {title:"Perfil usuario","usuario":req.cookies.cookieuser})}
-    else if(req.session.usuario){
-        return res.render ('users/login', {title:"Perfil usuario","usuario":req.session.usuario})
-    }
->>>>>>> 78406b307854ff149f3341caaaaf5d41cb2dac89
   next();
 };
