5195a410a3a000c9b58ee5db90d3039fa1b3d650
Guillermina <guillecipriaani@gmail.com>
2020-08-05 17:22:14 -0300

    cambios en vista home, falta terminar

--- site/dashboard/src/components/Main.js
+++ site/dashboard/src/components/Main.js
@@ -32,13 +32,8 @@ class Main extends Component {
     return (
         <div className="container-fluid">
 
-        {/* <!-- Page Heading --> */}
-        <div className="d-sm-flex align-items-center justify-content-between mb-4">
-            <h1 className="h3 mb-0 text-gray-800">App Dashboard</h1>
-        </div>
-        
         {/* <!-- Content Row --> */}
-        <div className="row mb-3 justify-content-between">
+        <div className="row mb-4 justify-content-between">
             {
                 this.state.cards.map((card, i)=> {
                     return (
@@ -48,20 +43,6 @@ class Main extends Component {
                     )
                 })
             }
-            {/* <!-- Amount of Products in DB --> */}
-            
-        
-            {/* <!-- $$$ of all products in DB --> */}
-            <div className="col-md-4 mb-4">
-                <Card color='success' icon="fa-clipboard-list" text='Amount in products' value='$546.456' />
-                
-            </div>
-        
-            {/* <!-- Amount of users in DB --> */}
-            <div className="col-md-4 mb-4">
-                <Card color='warning' icon="fa-clipboard-list" text='Users quantity' value='38' />
-                
-            </div>
         </div>
         
         {/* <!-- Content Row --> */}
